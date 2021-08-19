551e605f9fffc309844b3ce135171edd75c4341a
Sandeepm2835 <89018296+Sandeepm2835@users.noreply.github.com>
2021-08-19 18:30:30 +0530

    Update signup.component.spec.ts

--- SamplePOC/src/app/auth/signup/signup.component.spec.ts
+++ SamplePOC/src/app/auth/signup/signup.component.spec.ts
@@ -1,16 +1,20 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ReactiveFormsModule } from '@angular/forms';
+import { AuthService } from '../auth.service';
 
 import { SignupComponent } from './signup.component';
 
-describe('SignupComponent', () => {
+xdescribe('SignupComponent', () => {
   let component: SignupComponent;
   let fixture: ComponentFixture<SignupComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ SignupComponent ]
+      imports: [ ReactiveFormsModule ],
+      declarations: [ SignupComponent ],
+      providers: [ AuthService ]
     })
-    .compileComponents();
+    .compileComponents(); 
   }));
 
   beforeEach(() => {
@@ -19,7 +23,36 @@ describe('SignupComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should check form with controls', () => {
+    expect(component.signupForm.contains('firstname')).toBeTruthy();
+    expect(component.signupForm.contains('lastname')).toBeTruthy();
+    expect(component.signupForm.contains('email')).toBeTruthy();
+    expect(component.signupForm.contains('phonenum')).toBeTruthy();
+    expect(component.signupForm.contains('address')).toBeTruthy();
+    expect(component.signupForm.contains('medhistory')).toBeTruthy();
+    expect(component.signupForm.contains('password')).toBeTruthy();
+    expect(component.signupForm.contains('cnfpassword')).toBeTruthy();
   });
+
+  it('should make for required validator', () => {
+    let fnameCntrl = component.signupForm.controls['firstname'];
+    expect(fnameCntrl.valid).toBeFalsy();
+
+    let errors = fnameCntrl.errors || {};
+    expect(errors['required']).toBeTruthy();
+
+    fnameCntrl.setValue('test');
+    errors = fnameCntrl.errors || {};
+    expect(errors['pattern']).toBeFalsy();
+  });
+
+  // it('should submit form', () => {
+  //   expect(component.signupForm.valid).toBeFalsy();
+
+  //   let fnameCntrl = component.signupForm.controls['firstname'].setValue('test');
+  //   expect(component.signupForm.valid).toBeTruthy();
+
+  // });
+
+
 });
